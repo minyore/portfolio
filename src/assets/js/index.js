170616478033de00b3e7d474dc54ef7d097e1648
웨이브_김민 <m.kim@wabe.co.kr>
2023-07-18 11:38:08 +0900

    portfolio update

---
+++ src/assets/js/index.js
@@ -0,0 +1,48 @@
+/**
+ * @author 김 민
+ */
+
+function init(){
+	informationLoad();
+	contentsLoad();
+	modalsLoad();
+	heightResize();
+}
+
+// height resize
+function heightResize(){
+	let windowHeight = window.innerHeight;
+	let documentHeight = $("html, body");
+	documentHeight.css('height', windowHeight);
+}
+
+// information component load
+function informationLoad(){
+	$("#information").load("/src/components/information/information.html");
+}
+
+// contents component load
+function contentsLoad(){
+	$("#contents").load("/src/components/contents/contents.html");
+}
+
+// modal component load
+function modalsLoad(){
+	$("#modals").load("/src/components/modal/index.html");
+}
+
+// information fold
+function handleFold(){
+	$(this).toggleClass("isFold");
+	$(".information").toggleClass("isFold");
+}
+
+// modal open
+function handleModalOpen(){
+	$(".modal").show();
+}
+
+// modal close
+function handleModalClose(){
+	$(".modal").hide();
+}
